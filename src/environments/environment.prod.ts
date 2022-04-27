904fcf044deafd4437ad402131d0ba248528f068
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2022-04-27 16:12:37 -0500

    w

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   /* Variables API */
   apiURL: 'https://api.rojuescenario5tienda.xyz/',
-  wsURL: 'ws://api.rojuescenario5tienda.xyz/',
+  wsURL: 'wss://api.rojuescenario5tienda.xyz/',
   url:null,
   blacktoken:false,
   ip:String,
